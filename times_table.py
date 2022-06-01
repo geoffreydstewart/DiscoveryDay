df16324b8b701a4917e9ae32bcd65a8d3cfd4d5a
Geoffrey Stewart <geoffreydstewart@hotmail.com>
2022-05-31 23:06:56 -0700

    Add some basic, initial exercises

---
+++ times_table.py
@@ -0,0 +1,9 @@
+
+# 1. Delete all the code, except for your multiply function.
+# 2. Type in the following code, below the multiply function to print the numbers from 0 to 12.
+
+for i in range(13):
+    print(f'i is {i} ')
+
+# 3. Using your multiply function can you print out a 9x table.
+# 4. ???

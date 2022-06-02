d440391ccf54001cb246fb1c155bf5730620a64d
Geoffrey Stewart <geoffreydstewart@hotmail.com>
2022-06-01 22:17:24 -0700

    second round of changes again

---
+++ starter5.py
@@ -0,0 +1,25 @@
+# THE PROGRAMMING PROJECT
+
+# 1. Change the function name from sum to add.
+
+# 2. In the renamed function, use a new variable to store the sum, and return this new variable.
+
+# 3. Just before the print statement, use a variable to store the result of calling the add function.
+
+# 4. change the print statement to output the sum using the new variable.
+
+# 5. Change this program to multiply two numbers.
+
+def multiply(a, b):
+    product = a * b
+    return product
+
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+total = multiply(a, b)
+print(f'Sum of {a} and {b} is {total}')
+
+
+
